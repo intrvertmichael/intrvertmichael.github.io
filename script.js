a359557227842a24c003b3b2cd5921c8ad6bfeee
Michael <michael@Michaels-MBP.fios-router.home>
2019-07-31 19:36:02 -0400

    updated code. updadated layout. new v than from glitch

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-$( ".menu" ).click(()=>{
-  $( ".container" ).toggleClass( "hidden" );
-  $( ".nav" ).toggleClass( "hidden" );
-  $( "body" ).toggleClass( "dark" );
-  $( "main" ).toggleClass( "wide" );
+const el = {
+  body: document.querySelector("body"),
+  navBtn: document.querySelector(".nav-btn"),
+  nav: document.querySelector("nav"),
+  main: document.querySelector("main")
+}
+
+el.navBtn.addEventListener('click', ()=>{
+  el.body.classList.toggle("dark");
+  el.nav.classList.toggle("hidden");
+  el.main.classList.toggle("hidden");
 });
