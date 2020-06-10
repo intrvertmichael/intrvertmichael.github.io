a93e72c3e7959169d001d1fc4e0dd4413c1dc19c
intrvertmichael <michaelpaguay@gmail.com>
2020-06-10 15:32:43 -0400

    build folder on github

---
+++ build/precache-manifest.e57d68a0a1e38d6c3ce3189be443105b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1416f74f91acdf0db4d2ff7fb86b7404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf93bc49dbea6cb3bf20",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7950665a2cc7c8a3e2b",
+    "url": "/static/js/2.5a31a37a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a31a37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf93bc49dbea6cb3bf20",
+    "url": "/static/js/main.0133ae22.chunk.js"
+  },
+  {
+    "revision": "54f30fcaea9500ce404a",
+    "url": "/static/js/runtime-main.b4c216f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
