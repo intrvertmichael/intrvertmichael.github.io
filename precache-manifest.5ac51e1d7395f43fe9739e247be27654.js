db2672a19fb6d22ba7b39819627ed3698527bfe0
intrvertmichael <michaelpaguay@gmail.com>
2020-08-10 19:46:45 -0400

    added slide up description

---
+++ precache-manifest.5ac51e1d7395f43fe9739e247be27654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ffa641e4b94e9c05f62a63cd6cfb47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bcede9deccbfa0f3a0",
+    "url": "/static/css/main.54cd0aad.chunk.css"
+  },
+  {
+    "revision": "29dc5f1f01049be9878e",
+    "url": "/static/js/2.72bd669b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72bd669b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bcede9deccbfa0f3a0",
+    "url": "/static/js/main.fc0ea2cf.chunk.js"
+  },
+  {
+    "revision": "54f30fcaea9500ce404a",
+    "url": "/static/js/runtime-main.b4c216f6.js"
+  }
+]);
